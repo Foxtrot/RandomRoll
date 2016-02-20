f01da29f39f4bd3999777cd2e051c1db38552572
Foxtrot <foxtrotnull@gmail.com>
2016-02-20 03:24:45 +0000

    add non ascii rickroll

---
+++ assets/Rolls/RickRoll/index.php
@@ -0,0 +1,17 @@
+<HTML>
+<head>
+<title>Never gonna give you up!</title> 
+</head> 
+<body bgcolor="black">
+<center>
+<img src="Rolls/RickRoll/asset/gif.gif">	
+</center>
+
+ <audio loop autoplay>
+ 	 <source src="Rolls/RickRoll/asset/song.mp3" type="audio/mpeg">
+  	 <source src="Rolls/RickRoll/asset/song.ogg" type="audio/ogg">
+ </audio>
+
+
+</body>
+</HTML>
